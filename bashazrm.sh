b9f1a2109afb3236d38a74a5533aa2cd400b7a8c
hn-88 <hn-88@users.noreply.github.com>
2021-07-17 16:23:53 +0530

    avoiding read works

--- bashazrm.sh
+++ bashazrm.sh
@@ -2,15 +2,16 @@
 input="./elist.txt"
 urlbeg="https://url/to/container/"
 urlend="?sp=racwdl&st=2021-06-30T04:56:20Z&se=2021-08-07T12:56:20Z&spr=https&sv=2020-08-04&sr=c&sig=signatureInSAStokenUc%3D"
-#i=1
-while IFS= read -r line
+i=1
+for line in $(cat elist.txt)
 do
   url="$urlbeg"
   url+="$line"
   url+="$urlend"
-  echo "Filename to delete is $line "
+  echo "$i. Filename to delete is $line "
   #read -p "Press Enter to continue" < /dev/tty
   /home/mac/Downloads/azcopy rm "$url"
   #echo "$url"
-  #i=$((i+1))
-done < "$input"
+  i=$((i+1))
+done
+
